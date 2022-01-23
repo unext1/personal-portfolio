936b5003dbbde42f1ef57c2e67aae5cad905c4be
Unext <45896882+unext1@users.noreply.github.com>
2022-01-23 19:45:33 +0100

    removed flowbite

--- pages/_document.js
+++ pages/_document.js
@@ -25,10 +25,6 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link
-            rel="stylesheet"
-            href="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/flowbite.min.css"
-          />
-          <link
             href="http://fonts.cdnfonts.com/css/sofia-pro"
             rel="stylesheet"
           />
